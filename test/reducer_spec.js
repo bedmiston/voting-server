8d9070557099c1932e7d8ed66ce94eb2eef25228
Brendon Edmiston <Brendon_Edmiston@reyrey.com>
2018-01-08 15:49:34 -0600

    Add a test to ensure vote can be used with a reducer

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -1,15 +1,8 @@
-import {
-  Map,
-  fromJS
-} from 'immutable';
-import {
-  expect
-} from 'chai';
+import { Map, fromJS } from 'immutable';
+import { expect } from 'chai';
 
 import reducer from '../src/reducer';
-import {
-  next
-} from '../src/core';
+import { next } from '../src/core';
 
 describe('reducer', () => {
   it('handles SET_ENTRIES', () => {
@@ -66,4 +59,19 @@ describe('reducer', () => {
     }));
   });
 
+  it('can be used with reduce', () => {
+    const actions = [
+      { type: 'SET_ENTRIES', entries: ['Trainspotting', '28 Days Later'] },
+      { type: 'NEXT' },
+      { type: 'VOTE', entry: 'Trainspotting' },
+      { type: 'VOTE', entry: '28 Days Later' },
+      { type: 'VOTE', entry: 'Trainspotting' },
+      { type: 'NEXT' }
+    ];
+    const finalState = actions.reduce(reducer, Map());
+
+    expect(finalState).to.equal(fromJS({
+      winner: 'Trainspotting'
+    }));
+  });
 });
